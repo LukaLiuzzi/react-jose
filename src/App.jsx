baa94526671d8e61d77dcf944ada8dfba41e1e56
Luka <lukaliuzzidev@gmail.com>
2024-03-05 15:44:34 -0300

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Navbar from "./components/Navbar"
 import Saludo from "./components/Saludo/Saludo"
 import Contador from "./components/Contador"
 
 function App() {
   const [contador, setContador] = useState(0)
+  const [nombre, setNombre] = useState("Luka")
+  const [tareas, setTareas] = useState([])
 
   const sumar = () => {
     setContador(contador + 1)
@@ -13,6 +15,27 @@ function App() {
   const restar = () => {
     setContador(contador - 1)
   }
+
+  // Solo se renderiza la primera vez que se ejecuta el componente
+  // useEffect(() => {}, [])
+
+  // Se renderiza cada vez que cambia el estado de contador o nombre
+  // useEffect(() => {}, [contador, nombre])
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://jsonplaceholder.typicode.com/todos")
+      const json = await response.json()
+      console.log(json)
+      setTareas(json)
+    }
+
+    fetchData()
+
+    return () => {
+      console.log("Adios")
+    }
+  }, [])
 
   return (
     <div>
@@ -23,6 +46,15 @@ function App() {
         <Saludo />
         <Saludo nombre={"otro"} />
         <Contador valor={contador} aumentar={sumar} restar={restar} />
+        {contador < 2 &&
+          tareas.map((tarea) => {
+            return (
+              <div key={tarea.id}>
+                <h2>{tarea.title}</h2>
+                <hr />
+              </div>
+            )
+          })}
       </main>
       <footer>
         <p>© 2021</p>
